bf694c23119899c6e4e48359238b7dbfab2ae2c7
lbilal0016 <leventbilal.16@gmail.com>
2023-12-16 21:40:18 +0100

    Client code updated for event functionality.

--- Client.cpp
+++ Client.cpp
@@ -28,15 +28,27 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     std::string ConfigFile(argv[1]);
+    //  Video configuration file is read for video library
     VideoReadWrite::ReadConfigFile(ConfigFile);
-
+    //  Video configuration file is read for vsomeip library
+    SomeIpLib::ReadConfigFile(ConfigFile);
 
     app = vsomeip::runtime::get()->create_application("Client");
     app->init();
     app->register_availability_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, on_availability);
     app->request_service(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID);
     app->register_message_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_METHOD_ID, on_message);
+
+    //  Creating a set of event groups
+    std::set<vsomeip::eventgroup_t> its_groups;
+    its_groups.insert(EVENT_GROUP_ID);   //  Adding group id
+    //  Offer event
+    app->offer_event(EVENT_SERVICE_ID, EVENT_INSTANCE_ID, EVENT_ID, its_groups, vsomeip_v3::event_type_e::ET_EVENT, 
+    std::chrono::milliseconds(1000),false,true,nullptr, vsomeip_v3::reliability_type_e::RT_UNKNOWN);
+      
     std::thread sender(run);
+    std::thread object_detection(run_detection);
     set_application(app);   //  Added for compatibility when client functions are packed in another object code.
     app->start();
+    object_detection.join();    //  Terminate the object_detection function somewhere
 }
