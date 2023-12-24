d4f950840824e4c2f25577d466cf779f917ae040
lbilal0016 <leventbilal.16@gmail.com>
2023-12-24 14:27:45 +0100

    Update for someip service library.

--- Includes/Someiplib/ServiceLib.cpp
+++ Includes/Someiplib/ServiceLib.cpp
@@ -10,6 +10,9 @@
 
 #include "VideoReadWrite.h"
 
+std::mutex mutex;
+std::condition_variable condition;
+
 //  This function passes the someip application object from the Client executable to this object code
 static std::shared_ptr<vsomeip::application> this_app;
 void set_application(std::shared_ptr<vsomeip::application> app)
@@ -21,9 +24,9 @@ void on_message(const std::shared_ptr<vsomeip::message>& Request)
 {
     std::cout << "on_message():\n";
     //  Reading the payload
-    std::shared_ptr<vsomeip::payload> its_payload = Request->get_payload();
+    std::shared_ptr<vsomeip::payload> response_payload = Request->get_payload();
     //  Getting the payload length
-    vsomeip::length_t len = its_payload->get_length();
+    vsomeip::length_t len = response_payload->get_length();
 
     //  Getting the payload
     std::stringstream ss;
@@ -31,11 +34,12 @@ void on_message(const std::shared_ptr<vsomeip::message>& Request)
     for(vsomeip::length_t i=0; i<len; ++i)
     {
         ss << std::setw(2) << std::setfill('0') << std::hex
-        //  its_payload->get_data() returns a pointer to the start of the payload data
+        //  response_payload->get_data() returns a pointer to the start of the payload data
         //  << " ";: A space is added after each formatted hexadecimal value to separate them in the final string.
-        <<(int)*(its_payload->get_data()+i) << " "; 
+        <<(int)*(response_payload->get_data()+i) << " "; 
     }
 
+    /*  ACKNOWLEDGE THE VIDEO REQUEST FROM THE CLIENT SIDE  */
     std::cout << "SERVICE: Received request with Client/Session [ "
     << std::setw(4) << std::setfill('0') << std::hex << Request->get_client() << "/"
     << std::setw(4) << std::setfill('0') << std::hex << Request->get_session() << "]"
@@ -43,20 +47,75 @@ void on_message(const std::shared_ptr<vsomeip::message>& Request)
 
     /*      READ THE INPUT FROM CONFIG AND PUT IT ON PAYLOAD    */
     //  Reading the input video file
-    //  log message
-    std::cout << "Server: Input file is being read ... \n";
+    std::cout << "Server: Input file is being read ... \n"; //  log message
     VideoData Payload_Video;    //  Create the container for video data 
     VideoRead(Payload_Video);   //  Read the input into this container for video data
 
-    //  Serialise video data and load it into an std::vector<uint8_t> object
+    /*  SERIALISE VIDEO DATA AND LOAD IT INTO AN STD::VECTOR<UINT8_T> OBJECT   */
     std::vector<uint8_t> raw_video = SerialiseVideoData(Payload_Video);
 
-    //  Create Response:
-    std::shared_ptr<vsomeip::message> its_response = vsomeip::runtime::get()->create_response(Request);
-    //      Refilling the its_payload object
-    its_payload = vsomeip::runtime::get()->create_payload();
+    /*  CREATE RESPONSE    */  
+    std::shared_ptr<vsomeip::message> response = vsomeip::runtime::get()->create_response(Request);
+    //      Refilling the response_payload object
+    response_payload = vsomeip::runtime::get()->create_payload();
 
-    its_payload->set_data(reinterpret_cast<const vsomeip::byte_t*>(raw_video.data()), raw_video.size());
-    its_response->set_payload(its_payload);
-    this_app->send(its_response);
+    response_payload->set_data(reinterpret_cast<const vsomeip::byte_t*>(raw_video.data()), raw_video.size());
+    response->set_payload(response_payload);
+    this_app->send(response);
+}
+
+void on_availability_event(vsomeip::service_t Service, vsomeip::instance_t Instance, bool is_available)
+{
+    std::cout << "SERVICE: Service[" << std::setw(4) << std::setfill('0') << std::hex << 
+    Service << "." << Instance << "] is " << (is_available ? "available." : "Not available.") << std::endl;
+
+    if(is_available)
+    {
+    //  Sending wake-up call for the waiting thread on the service side after the detection event becomes available on the client side
+    std::cout << "SERVICE: Condition lock will now be removed, since Service[" << std::setw(4) << std::setfill('0') 
+    << Service << "." << Instance << "] is available now." << std::endl;
+    
+    condition.notify_one();
+    }
+}
+
+void on_message_event(const std::shared_ptr<vsomeip::message>& event_message)
+{
+    /*  ACKNOWLEDGE EVENT_MESSAGE */
+
+   std::stringstream message;
+
+   message << "SERVICE: A notification for event [" << 
+   std::setw(4) << std::setfill('0') << std::hex <<
+   event_message->get_service() << "." << 
+   event_message->get_instance() << "." <<
+   std::setw(4) << std::setfill('0') << std::hex <<
+   event_message->get_method() << "] to ClientID/Session [" <<
+   std::setw(4) << std::setfill('0') << std::hex <<
+   event_message->get_client() <<
+   std::setw(4) << std::setfill('0') << std::hex <<
+   event_message->get_session() << "]" << std::endl;
+
+    /*  WRITE THE DATA FROM EVENT_MESSAGE INTO AN OBJECT_TYPE_T VARIABLE  */
+    std::shared_ptr<vsomeip::payload> message_payload = event_message->get_payload();
+
+    object_type_t detected_object;
+    detected_object.type = (char)*(message_payload->get_data());
+    detected_object.count = (int)*(message_payload->get_data() + 1);
+    detected_object.time = (float)*(message_payload->get_data() + 2);
+
+    Detection_Object Captured_object(detected_object);
+    Captured_object.print_object();
+    Captured_object.~Detection_Object();
+}
+
+void run_events()
+{
+    std::unique_lock<std::mutex> lock_events(mutex);
+    condition.wait(lock_events);
+
+    std::set<vsomeip::eventgroup_t> event_groups;
+    event_groups.insert(EVENT_GROUP_ID);
+    this_app->request_event(EVENT_SERVICE_ID, EVENT_INSTANCE_ID,EVENT_ID, event_groups, vsomeip::event_type_e::ET_UNKNOWN);
+    this_app->subscribe(EVENT_SERVICE_ID, EVENT_INSTANCE_ID, EVENT_GROUP_ID);
 }
--- Includes/Someiplib/ServiceLib.h
+++ Includes/Someiplib/ServiceLib.h
@@ -16,7 +16,21 @@
 #define SAMPLE_INSTANCE_ID 0x5677
 #define SAMPLE_METHOD_ID 0x0421
 
+/*  IDS FOR OBJECT DETECTION EVENT COMMUNICATION    */
+#define EVENT_ID 0x8778
+#define EVENT_SERVICE_ID 0x1000
+#define EVENT_INSTANCE_ID 0x3000
+#define EVENT_GROUP_ID 0x4465
+
+using Detection_Object = VideoReadWrite::Detection_Object;
+
 void on_message(const std::shared_ptr<vsomeip::message>& Request);
+
 void set_application(std::shared_ptr<vsomeip::application> app);
+
+void on_availability_event(vsomeip::service_t Service, vsomeip::instance_t Instance, bool is_available);
+void on_message_event(const std::shared_ptr<vsomeip::message>& event_message);
+
+void run_events();
 
 #endif
